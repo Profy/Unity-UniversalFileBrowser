56267a9c37d27d27eafee58a5ca699ec0543d9b7
Profy <marc.aubry.boissel@gmail.com>
2022-06-19 18:36:51 +0200

    Plugins build batch command

---
+++ Native/Windows/ShellFileDialogs/build.cmd
@@ -0,0 +1,2 @@
+msbuild.exe ShellFileDialogs.sln /m /p:Configuration=Release;Platform=x64 /p:OutputPath="..\..\..\..\..\Unity\UniversalFileBrowser\Assets\Plugins\UniversalFileBrowser\Windows64"
+msbuild.exe ShellFileDialogs.sln /m /p:Configuration=Release;Platform=x86 /p:OutputPath="..\..\..\..\..\Unity\UniversalFileBrowser\Assets\Plugins\UniversalFileBrowser\Windows32"
